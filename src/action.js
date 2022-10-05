8fa1ec26f23c91c765d38b4a079eb60882f25eec
gjack <gabi@ministrycentered.com>
2022-10-05 09:58:27 -0400

    Add dependency @octokit/action. Use octokit to find all branches corresponding to head_commit

--- src/action.js
+++ src/action.js
@@ -1,1 +1,24 @@
-console.log("Testing action...")
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { Octokit } = require("@octokit/action");
+
+async function run() {
+  try {
+    const octokit = new Octokit();
+
+    const context = github.context
+    const payload = context.payload
+
+    // Get all branches for head_commit in payload
+    const commit_sha = payload.head_commit.id
+    const branches = octokit.rest.repos.listBranchesForHeadCommit({
+      ...context.repo,
+      commit_sha,
+    });
+
+  } catch(error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
