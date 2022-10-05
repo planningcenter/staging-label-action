022770ba3db46d7114041aeef002e11eaf33689b
gjack <gabi@ministrycentered.com>
2022-10-05 10:07:17 -0400

    Use await with octokit methods. Find list of pull requests in repository

--- src/action.js
+++ src/action.js
@@ -11,10 +11,11 @@ async function run() {
 
     // Get all branches for head_commit in payload
     const commit_sha = payload.head_commit.id
-    const branches = octokit.rest.repos.listBranchesForHeadCommit({
+    const { data: branches } = await octokit.rest.repos.listBranchesForHeadCommit({
       ...context.repo,
       commit_sha,
     });
+    const { data: pulls } = await octokit.rest.pulls.list({ ...context.repo })
 
   } catch(error) {
     core.setFailed(error.message);
